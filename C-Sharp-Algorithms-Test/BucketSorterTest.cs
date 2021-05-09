f00b4f7da732a0ec37b86162afe51e295c8a07ff
mamn <mamn@vitronic.com>
2021-05-09 14:21:17 +0400

    add unit testing for bucket sorter

---
+++ C-Sharp-Algorithms-Test/BucketSorterTest.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Search;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace C_Sharp_Algorithms_Test
+{
+    public class BucketSorterTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var list = new List<int> { 23, 42, 4, 16, 8, 15, 3, 9, 55, 0, 34, 12, 2, 46, 25 };
+            BucketSorter.BucketSort(list);
+        }
+        [Fact]
+        public void Test2()
+        {
+            var list = new List<int> { 23, 42, 4, 16, 8, 15, 3, 9, 55, 0, 34, 12, 2, 46, 25 };
+            BucketSorter.BucketSortAscending(list);
+        }
+        [Fact]
+        public void Test3()
+        {
+            var list = new List<int> { 23, 42, 4, 16, 8, 15, 3, 9, 55, 0, 34, 12, 2, 46, 25 };
+            BucketSorter.BucketSortDescending(list);
+        }
+    }
+}
