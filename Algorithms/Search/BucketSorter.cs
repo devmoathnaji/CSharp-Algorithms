40db89950127def6b336fddbf21964af9d556853
mamn <mamn@vitronic.com>
2021-05-09 14:18:09 +0400

    fixes, add Bucket Sorter

---
+++ Algorithms/Search/BucketSorter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Search
+{
+    public static class BucketSorter
+    {
+        public static void BucketSort(this IList<int> collection)
+        {
+            collection.BucketSortAscending();
+        }
+        public static void BucketSortAscending(this IList<int> collection)
+        {
+            int maxValue = collection.Max();
+            int minValue = collection.Min();
+            List<int>[] bucket = new List<int>[maxValue - minValue + 1];
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                bucket[i] = new List<int>();
+            }
+            foreach (int i in collection)
+            {
+                bucket[i - minValue].Add(i);
+            }
+            int k = 0;
+            foreach (List<int> item in bucket)
+            {
+                if (item.Count > 0)
+                {
+                    foreach (var j in item)
+                    {
+                        collection[k] = j;
+                        k++;
+                    }
+                }
+            }
+        }
+        public static void BucketSortDescending(this IList<int> colloection)
+        {
+            int maxValue = colloection[0];
+            int minValue = colloection[0];
+            for (int i = 1; i < colloection.Count; i++)
+            {
+                if (colloection[i] > maxValue)
+                    maxValue = colloection[i];
+                if (colloection[i] < minValue)
+                    minValue = colloection[i];
+            }
+            List<int>[] bucket = new List<int>[maxValue - minValue + 1];
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                bucket[i] = new List<int>();
+            }
+            int k = colloection.Count - 1;
+            foreach (List<int> i in bucket)
+            {
+                if (i.Count > 0)
+                {
+                    foreach (int j in i)
+                    {
+                        colloection[k] = j;
+                        k--;
+                    }
+                }
+            }
+        }
+    }
+}
--- C-Sharp-Algorithms-Test/BubbleSorterTest.cs
+++ C-Sharp-Algorithms-Test/BubbleSorterTest.cs
@@ -18,5 +18,26 @@ namespace C_Sharp_Algorithms_Test
                 list
                 );
         }
+
+        [Fact]
+        public void Test2()
+        {
+            var list = new List<int> { 23, 42, 4, 16, 8, 15, 3, 9, 55, 0, 34, 12, 2, 46, 25 };
+            BubbleSorter.BubbleSortAscending<int>(list, Comparer<int>.Default);
+            Assert.Equal(
+                new List<int> { 0, 2, 3, 4, 8, 9, 12, 15, 16, 23, 25, 34, 42, 46, 55 },
+                list
+                );
+        }
+        [Fact]
+        public void Test3()
+        {
+            var list = new List<int> { 23, 42, 4, 16, 8, 15, 3, 9, 55, 0, 34, 12, 2, 46, 25 };
+            BubbleSorter.BubbleSortDescending<int>(list, Comparer<int>.Default);
+            Assert.Equal(
+                new List<int> { 0, 2, 3, 4, 8, 9, 12, 15, 16, 23, 25, 34, 42, 46, 55 },
+                list
+                );
+        }
     }
 }
