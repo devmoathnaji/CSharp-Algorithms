21d4416a529cc35f2a46be43f76741b72a1d6aa7
mamn <mamn@vitronic.com>
2021-05-09 13:46:49 +0400

    add helpers swap and populate method

---
+++ Algorithms/Common/Helpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Common
+{
+    public static class Helpers
+    {
+        public static void Swap<T>(this IList<T> list, int firstIndex, int secoundIndex)
+        {
+            if (list.Count < 2 || firstIndex == secoundIndex)
+                return;
+            var temp = list[firstIndex];
+            list[firstIndex] = list[secoundIndex];
+            list[secoundIndex] = temp;
+        }
+        public static void Swap<T>(this List<T> list, int firstIndex, int secoundIndex)
+        {
+            if (list.Count < 2 || firstIndex == secoundIndex)
+                return;
+            var temp = list[firstIndex];
+            list[firstIndex] = list[secoundIndex];
+            list[secoundIndex] = temp;
+        }
+        public static void Populates<T>(this IList<T> collection, T value)
+        {
+            if (collection == null)
+                return;
+            for (int i = 0; i < collection.Count; i++)
+            {
+                collection[i] = value;
+            }
+          
+        }
+    }
+}
---
+++ Algorithms/Search/BubbleSorter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Search
+{
+    public class BubbleSorter
+    {
+        public static void BubbleSort<T>(this IList<T> collection, Comparer<T> comparer = null)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+            collection.BubbleSortAscending(comparer);
+        }
+        public static void BubbleSortAscending<T>(this IList<T> collection, Comparer<T> comparer)
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                for (int index = 0; index < collection.Count - i; index++)
+                {
+                    if (comparer.Compare(collection[index], collection[index + 1]) > 0) 
+                    {
+                        collection.Swap(index, index + 1);
+                    }
+                }
+            }
+        }
+        public static void BubbleSortDescending<T>(this IList<T> collection, Comparer<T> comparer)
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                for (int index = 0; index < collection.Count; index++)
+                {
+                    collection.Swap(index - 1, index);
+                }
+
+            }
+        }
+
+    }
+}
