9d23278181f72a426ec26bb5f5366ba176cf2fed
mamn <mamn@vitronic.com>
2021-05-10 15:53:13 +0400

    Gnom Sorter and test calss

---
+++ Algorithms/Sorter/GnomeSorter.cs
@@ -0,0 +1,48 @@
+﻿using Algorithms.Common;
+using System.Collections.Generic;
+
+namespace Algorithms.Sorter
+{
+    public static class GnomeSorter
+    {
+        public static void GnomeSort<T>(this IList<T> collection, Comparer<T> comparer = null)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+        }
+        public static void GnomeSortAscending<T>(this IList<T> collection, Comparer<T> comparer)
+        {
+            int pos = 1;
+            while (pos < collection.Count)
+            {
+                if (comparer.Compare(collection[pos], collection[pos - 1]) >= 1)
+                {
+                    pos++;
+                }
+                else
+                {
+                    collection.Swap(pos, pos - 1);
+                    if (pos > 1)
+                        pos--;
+                }
+            }
+        }
+        public static void GnomeSortDescending<T>(this IList<T> collection, Comparer<T> comparer)
+        {
+            int pos = 1;
+            while (pos < collection.Count)
+            {
+                if (comparer.Compare(collection[pos], collection[pos - 1]) <= 0)
+                {
+                    pos++;
+                }
+                else
+                {
+                    collection.Swap(pos, pos - 1);
+                    if (pos > 1)
+                        pos--;
+                }
+            }
+        }
+
+    }
+}
---
+++ C-Sharp-Algorithms-Test/GnomeSorterTest.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Sorter;
+using System.Collections.Generic;
+using Xunit;
+
+namespace C_Sharp_Algorithms_Test
+{
+    public class GnomeSorterTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var list = new List<int>() { 8, 9, 6, 2, 41, 54, 69, 5, 9, 5, 99, 5, 55, 5, 5858, 100, 6 };
+            GnomeSorter.GnomeSort<int>(list, Comparer<int>.Default);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var list = new List<int>() { 8, 9, 6, 2, 41, 54, 69, 5, 9, 5, 99, 5, 55, 5, 5858, 100, 6 };
+            GnomeSorter.GnomeSortDescending<int>(list, Comparer<int>.Default);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var list = new List<int>() { 8, 9, 6, 2, 41, 54, 69, 5, 9, 5, 99, 5, 55, 5, 5858, 100, 6 };
+            GnomeSorter.GnomeSortAscending<int>(list, Comparer<int>.Default);
+        }
+    }
+}
