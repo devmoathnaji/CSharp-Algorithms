2f5929bff346524da7a96ecd1c328263573b2b01
mamn <mamn@vitronic.com>
2021-05-10 15:25:21 +0400

    Counting Sorter with examples

---
+++ Algorithms/Search/CountingSorter.cs
@@ -0,0 +1,47 @@
+﻿using Algorithms.Common;
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Search
+{
+    public static class CountingSorter
+    {
+        public static void CountingSort(this IList<int> collection)
+        {
+            if (collection == null || collection.Count == 0)
+                return;
+            //get the max number in array
+            int maxK = 0;
+            int index = 0;
+            while (true)
+            {
+                if (index >= collection.Count)
+                    break;
+                maxK = Math.Max(maxK, collection[index] + 1);
+                index++;
+            }
+            int[] keys = new int[maxK];
+            keys.Populates(0); //Populates it with zeros
+            //assign the keys 
+            for (int i = 0; i < collection.Count; i++)
+            {
+                keys[collection[i]] += 1;
+            }
+            //reset index
+            index = 0;
+            //sort the elements 
+            for (int j = 0; j < keys.Length; j++)
+            {
+                var val = keys[j];
+                if (val > 0)
+                {
+                    while (val-- > 0)
+                    {
+                        collection[index] = j;
+                        index++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C-Sharp-Algorithms-Test/CountingSorterTest.cs
@@ -0,0 +1,16 @@
+﻿using Algorithms.Search;
+using System.Collections.Generic;
+using Xunit;
+
+namespace C_Sharp_Algorithms_Test
+{
+    public class CountingSorterTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var list = new List<int> { 8, 5, 1, 2, 5, 4, 7, 5, 2, 1, 2, 1 };
+            CountingSorter.CountingSort(list);
+        }
+    }
+}
