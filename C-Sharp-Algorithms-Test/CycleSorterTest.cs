184f991a2adb79175325f5788d13b64618252aba
mamn <mamn@vitronic.com>
2021-05-10 15:40:24 +0400

    Cycle Sorter and test class

---
+++ C-Sharp-Algorithms-Test/CycleSorterTest.cs
@@ -0,0 +1,16 @@
+﻿using Algorithms.Sorter;
+using System.Collections.Generic;
+using Xunit;
+
+namespace C_Sharp_Algorithms_Test
+{
+    public  class CycleSorterTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var list = new List<int>() { 8, 5, 5, 2, 1, 4, 8, 54, 5, 1, 5, 2, 21, 55 };
+            CycleSorter.CycleSort<int>(list, null);
+        }
+    }
+}
